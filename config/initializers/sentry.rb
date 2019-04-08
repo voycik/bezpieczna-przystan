41373890a8ffce0edeed10057be9a47ea62a62ad
jluczak <jluczaksas@gmail.com>
2019-04-08 15:40:01 +0200

    Rubocop corrections

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Raven.configure do |config|
   config.dsn = 'https://9d6b03e30df34107b080bd8b4dd8eb7a:06c140bf60fd48c2af1ced9935701a5f@sentry.io/1433732'
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
