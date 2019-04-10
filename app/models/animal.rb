ad0566e898e875ef88eb22e0dc337e10ad99f2f7
jluczak <jluczaksas@gmail.com>
2019-04-10 17:50:23 +0200

    Fix for_kids validation

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,7 +3,7 @@ class Animal < ApplicationRecord
   validates :gender, presence: true
   validates :size, presence: true
   validates :purpose, presence: true
-  validates :for_kids, presence: true
+  validates :for_kids, inclusion: { in: [ true, false ] }
   validates :age, presence: true, numericality: { less_than_or_equal_to: 30,  only_integer: true }
   validates :general_info, length: { maximum: 1000 }
   validates :come_date, presence: true
