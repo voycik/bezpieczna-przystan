d39266e0073e49396e9bc5a8dd2dd43838cebbd0
jluczak <jluczaksas@gmail.com>
2019-04-10 17:36:08 +0200

    Add breed attribute to animals

---
+++ db/migrate/20190410153453_add_breed_to_animals.rb
@@ -0,0 +1,5 @@
+class AddBreedToAnimals < ActiveRecord::Migration[5.2]
+  def change
+    add_column :animals, :breed, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_123708) do
+ActiveRecord::Schema.define(version: 2019_04_10_153453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_123708) do
     t.date "sterilization_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "breed"
   end
 
   create_table "articles", force: :cascade do |t|
