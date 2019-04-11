a32dde3c01231d90586371dfdb86c5fd1b3e1e37
voycik <voycik@gmail.com>
2019-04-11 18:47:59 +0200

    Add photo attribute validation for animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -10,4 +10,6 @@ class Animal < ApplicationRecord
   validates :general_info, length: { maximum: 1000 }
   validates :come_date, presence: true
   validates :breed, presence: true
+  validates :photo, presence: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'Nieobsługiwany format. Wybierz plik .gif, .jpg, lub .png' }
+
 end
