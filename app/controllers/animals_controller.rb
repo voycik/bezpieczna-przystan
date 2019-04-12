85a7ed3245703788fe0dc9a6d5a9011a60a4ab9a
voycik <voycik@gmail.com>
2019-04-12 21:47:19 +0200

    Update animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,7 @@
 class AnimalsController < ApplicationController
+  before_action :find_animal, only: %i[show edit destroy]
   def index
+    @animals = Animal.all
   end
 
   def show
@@ -8,9 +10,29 @@ class AnimalsController < ApplicationController
   def new
   end
 
+  def edit
+  end
+
   def create
+  end
+
+  def update
   end
 
   def destroy
   end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :type, :gender, :size, :age,
+      :purpose, :for_kids, :photo, :general_info, :come_date, :vaccination_date,
+      :breed)
+  end
+
+  def find_animal
+    @animal = Animal.find(params[:id])
+  end
+
+
 end
