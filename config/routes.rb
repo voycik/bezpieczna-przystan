7a839b3cbc0f03bc751e84e52c126414911f4f46
voycik <voycik@gmail.com>
2019-04-12 10:04:16 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'animals/index'
-  get 'animals/show'
-  get 'animals/new'
-  get 'animals/create'
-  get 'animals/destroy'
+  resources :animals, :articles
   root 'articles#index'
-  get 'articles/index'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/edit'
-  get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
