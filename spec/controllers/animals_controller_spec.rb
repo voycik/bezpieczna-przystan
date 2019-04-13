da54e17c8952951e60ca4b7f3ee51f6e30840f3a
voycik <voycik@gmail.com>
2019-04-13 12:19:10 +0200

    Comment out wrong spec

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -17,13 +17,13 @@ RSpec.describe AnimalsController, type: :controller do
   #     expect(response).to have_http_status(:success)
   #   end
   # end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  #
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
   # describe "GET #create" do
   #   it "returns http success" do
