a0bc7cc1692a729ab7375f4d867b4af62f35dffb
jluczak <jluczaksas@gmail.com>
2019-04-13 22:08:13 +0200

    Fill in actions for Articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,51 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.all
+  end
 
-  def new; end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-  def create; end
+  def new
+    @article = Article.new
+  end
 
-  def edit; end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Post opublikowany"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
 
-  def destroy; end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    if @article.update_attributes(article_params)
+      flash[:success] = "Post zaktualizowany"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:success] = "Post usunięty"
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :photo)
+  end
 end
