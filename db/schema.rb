a20d4c5bf57ad48edd193113870adebacff14af0
jluczak <jluczaksas@gmail.com>
2019-04-17 11:53:40 +0200

    Rubocop corrections

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,54 +12,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_200621) do
-
+ActiveRecord::Schema.define(version: 20_190_414_200_621) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "animals", force: :cascade do |t|
-    t.string "name", limit: 30, null: false
-    t.string "type", null: false
-    t.string "gender", null: false
-    t.string "size", null: false
-    t.string "purpose", null: false
-    t.boolean "for_kids"
-    t.integer "age", null: false
-    t.text "general_info"
-    t.date "come_date", null: false
-    t.date "vaccination_date"
-    t.date "sterilization_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "breed", null: false
+  create_table 'animals', force: :cascade do |t|
+    t.string 'name', limit: 30, null: false
+    t.string 'type', null: false
+    t.string 'gender', null: false
+    t.string 'size', null: false
+    t.string 'purpose', null: false
+    t.boolean 'for_kids'
+    t.integer 'age', null: false
+    t.text 'general_info'
+    t.date 'come_date', null: false
+    t.date 'vaccination_date'
+    t.date 'sterilization_date'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'breed', null: false
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'body', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string "image", null: false
-    t.string "imageable_type"
-    t.bigint "imageable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
+  create_table 'images', force: :cascade do |t|
+    t.string 'image', null: false
+    t.string 'imageable_type'
+    t.bigint 'imageable_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[imageable_type imageable_id], name: 'index_images_on_imageable_type_and_imageable_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
