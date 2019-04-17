4c41306a55d6bb6911d1bfec4aeb3928978393b8
voycik <voycik@gmail.com>
2019-04-17 18:51:24 +0200

    Create messeges controller and setup routes

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.valid?
+      MessageMailer.contact(@message).deliver_later
+      redirect_to new_message_url
+      flash[:notice] = "Dziękujemy za Twoją wiadomość, odezwiemy się najszybciej jak to możliwe."
+    else
+      flash[:notice] = "Nie udało się wysłać wiadomości. Sprawdź błędy i spróbuj ponownie."
+      render :new
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :phone_number, :body)
+  end
+end
