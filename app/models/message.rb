496c683580a72ac3c6376d3d21b0babd3a424939
voycik <voycik@gmail.com>
2019-04-17 18:23:59 +0200

    Create message model (no migration)

---
+++ app/models/message.rb
@@ -0,0 +1,9 @@
+class Message
+
+  include ActiveModel::Model
+  attr_accessor :name, :email, :phone_number, :body
+  validates :name, :email, :body, presence: true
+  validates :phone_number, numericality: { only_integer: true }
+  validates :email, email: true
+
+end
