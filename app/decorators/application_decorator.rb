0f55fb182381d367c82c5bb13cf6e21f82815f37
voycik <voycik@gmail.com>
2019-04-19 18:34:41 +0200

    Separate decorators for arictles and animals

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,8 +1,4 @@
-class ApplicationDecorator < Draper::Decorator
-
-  def main_image
-    !!object.photo ? object.photo : object.images.any? ? object.images.first.image_url : "https://dummyimage.com/300x200/a3a3a3/fff.jpg&text=Brak+zdj%C4%99cia"
-  end
+class ApplicationDecorator < Draper::Decorator  
 
   def self.collection_decorator_class
     PaginatingDecorator
