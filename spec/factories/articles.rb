abc54bda360dd6bbb1e867783ec0b5677e760edf
jluczak <jluczaksas@gmail.com>
2019-04-19 17:11:56 +0200

    Image upload tests + image fixtures

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
   factory :article do
     title { Faker::Lorem.sentence }
     body { Faker::Lorem.paragraphs(3) }
-    # photo { 'Art.jpg' }
   end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,5 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include CarrierWave::Test::Matchers
+
+  before do
+    ImageUploader.enable_processing = true
+  end
+
+  describe 'image versions' do
+    before do
+      @article = FactoryBot.create(:article)
+      @article.images.create!(image: File.open('spec/fixtures/test_image.jpg'))
+    end
+
+    # describe '.small' do
+    #   it "scales down an image to be exactly 100 by 100 pixels" do
+    #     expect(@article.images.last.image_url).to have_dimensions(100, 100)
+    #   end
+    # end
+    #
+    # describe '.large' do
+    #   it "scales down a landscape image to be no larger than 400 by 400 pixels" do
+    #     expect(@article.images.last.image_url(:large)).to be_no_larger_than(400, 400)
+    #   end
+    # end
+
+    after do
+      ImageUploader.enable_processing = false
+    end
+  end
 end
