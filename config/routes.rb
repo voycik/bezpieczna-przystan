7a862a32e19eb98d0ae72454996100f8fe060098
voycik <voycik@gmail.com>
2019-04-20 17:09:09 +0200

    Setup routes for dog adoption form

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
     get 'admin', to: 'devise/sessions#new'
     post 'admin', to: 'devise/sessions#create'
   end
-  resources :animals, :articles
+  resources :animals, except: %i[adoption send_adoption_form]
+  resources :articles
   root 'articles#index'
   get 'contact-me', to: 'messages#new', as: 'new_message'
   post 'contact-me', to: 'messages#create', as: 'create_message'
+  get 'adopt', to: 'animals#adoption', as: 'adoption'
+  post 'adopt', to: 'animals#send_adoption_form', as: 'send_adoption_form'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
